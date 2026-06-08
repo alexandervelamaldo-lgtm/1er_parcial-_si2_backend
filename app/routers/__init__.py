1036ddd17a88df713f6811cc591480b290530860
alexander vela yayo <alexandervelamaldo@gmail.com>
2026-06-08 07:38:48 -0400

    Update __init__.py

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,18 +1,1 @@
-from . import inteligencia_automatizacion, pagos_facturacion
-from .autenticacion_acceso import auth
-from .gestion_operativa_web import clientes, notificaciones, talleres, tecnicos
-from .gestion_solicitudes import solicitudes, vehiculos
-from .seguimiento_cliente_web import mapa
-
-__all__ = [
-    "auth",
-    "inteligencia_automatizacion",
-    "clientes",
-    "mapa",
-    "notificaciones",
-    "pagos_facturacion",
-    "solicitudes",
-    "talleres",
-    "tecnicos",
-    "vehiculos",
-]
+__all__ = []
